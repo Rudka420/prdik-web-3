dc9bcda8ca8d22613938db9d6efc5100899e9317
Rudka420 <vrsovsky.r@gmail.com>
2024-09-05 22:13:39 +0200

    Přidání konfigurace pro načítání obrázků

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['prdik-web-2.vercel.app'], // nahraď názvem tvého projektu na Vercelu
+    },
+  };
+  
+  export default nextConfig;
+  
